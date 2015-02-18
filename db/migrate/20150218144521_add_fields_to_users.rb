1534f3e6d0574e38eb34d59d90d8bf15ceef0feb
am.valeh <hooman_mostafavi@yahoo.com>
2015-02-18 18:34:54 +0330

    migration added for extra fields in users

---
+++ db/migrate/20150218144521_add_fields_to_users.rb
@@ -0,0 +1,16 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :nickname, :string
+    add_column :users, :birth_date, :date
+
+    add_index :users, :confirmation_token,  :unique => true
+
+  end
+end
+
