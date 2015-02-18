68acdc3b9b4519a821dc5b108ea8ff680773b3f5
am.valeh <hooman_mostafavi@yahoo.com>
2015-02-18 20:35:05 +0330

    birth_date added to registration using jalalidate gem + first_name,last_name,nickname

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,48 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  include ApplicationHelper
+
+  before_action :set_date_select, [:create, :update]
+
+  def create
+    build_resource(sign_up_params)
+
+    persian_date = JalaliDate.new(persian_to_english_num_string(params[:birth_year]).to_i,params[:birth_month].to_i,
+                                  persian_to_english_num_string(params[:birth_day]).to_i)
+    resource.birth_date = persian_date.to_g
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords
+      respond_with resource
+    end
+  end
+
+  private
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :nickname, :birth_date, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :nickname, :birth_date, :email, :password, :password_confirmation, :current_password)
+  end
+
+  def set_date_select
+    @years = Hash.new
+    ((persianNumber('1300', 'fa')..persianNumber('1393','fa')).to_a.reverse).to_a.each { |e| @years[e] = e }
+    @days = Hash.new
+    (persianNumber('1', 'fa')..persianNumber('31','fa')).to_a.each { |e| @days[e] = e }
+    @months = Hash.new
+    @months = { 1 => 'فروردین ',2 => 'اردیبهشت',3 => 'خرداد' ,4 => 'تیر',5 => 'مرداد' ,6 => 'شهریور' ,7 => 'مهر'  ,8 => 'آبان' ,9 => 'آذر'  ,10 => 'دی' ,11 => 'بهمن' ,12 => 'اسفند'}
+  end
+
+
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+  def persian_to_english_num_string(str)
+    str.tr('۰۲۳۴۵۶۷۸۹۱','0234567891')
+  end
+
+  def persianNumber (to_convert, locale=nil, text = nil)
+    to_convert = to_convert.to_i.to_s
+    case locale
+      when 'ar'
+        to_convert = to_convert.unpack('U*').map{ |e| e + 1584 }.pack('U*')
+        text ? to_convert + ' ' + text : to_convert
+      when 'fa'
+        to_convert = to_convert.unpack('U*').map{ |e| e + 1728 }.pack('U*')
+        text ? to_convert + ' ' + text : to_convert
+      when 'hi'
+        to_convert = to_convert.unpack('U*').map{ |e| e + 2358 }.pack('U*')
+        text ? to_convert + ' ' + text : to_convert
+      else
+        to_convert = to_convert.unpack('U*').map{ |e| e + 1728 }.pack('U*')
+        text ? to_convert + ' ' + text : to_convert
+    end
+  end
+
 end
